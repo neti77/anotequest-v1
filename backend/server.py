2784def71bc6a81517d466b86f31809b4cf9fe2f
neti <167762876+neti77@users.noreply.github.com>
2025-12-26 12:28:47 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -17,9 +17,15 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.getenv("MONGO_URL")
+db_name = os.getenv("DB_NAME")
+
+if not mongo_url or not db_name:
+    raise RuntimeError("MONGO_URL or DB_NAME environment variables are not set")
+
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+db = client[db_name]
+
 
 # Create the main app without a prefix
 app = FastAPI()
