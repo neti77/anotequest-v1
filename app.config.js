1f4a82aa2a93a0a75f38821dcec05df1bc5ac959
neti <zzoiarts@gmail.com>
2026-01-07 22:25:05 +0100

    date 7 jan

--- app.config.js
+++
@@ -1,9 +0,0 @@
-export default ({ config }) => ({
-  ...config,
-  name: 'AnoteQuest Mobile',
-  slug: 'anotequest-mobile',
-  version: '0.0.1',
-  extra: {
-    REACT_APP_API_URL: process.env.REACT_APP_API_URL || ''
-  }
-});
